ac88f0ebeda32f3f57875e1fc543c0cd68cdf81c
Kolesnik Valeria <kolesnik.va8@gmail.com>
2024-02-13 16:07:10 +0000

    integrated redux and added imports

--- src/index.js
+++ src/index.js
@@ -1,10 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux'
-import './index.css';
-import App from './App';
 import { store } from './store/store'
-
+import App from './App';
+import './index.css';
 
 ReactDOM.render(
   <React.StrictMode>
