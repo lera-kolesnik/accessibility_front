970a2fba64f1a7befbc07603bef3f7fa2c9a4e66
Kolesnik Valeria <kolesnik.va8@gmail.com>
2024-02-13 15:53:53 +0000

    added routing with both main and about-us routes

--- src/App.jsx
+++ src/App.jsx
@@ -1,22 +1,19 @@
-import "./App.css";
-import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
-import Header from "./components/Header";
-import Main from "./components/Main";
+import './App.css'
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom'
+import Header from './components/Header'
+import Main from './components/Main'
+import About from './components/About'
 
-const A = () => <div>О нас</div>;
-
-function App() {
+export default function App() {
   return (
     <Router>
       <div className="App">
         <Header />
         <Routes>
           <Route path="/" element={<Main />} />
-          <Route path="/about-us" element={<A />} />
+          <Route path="/about-us" element={<About />} />
         </Routes>
       </div>
     </Router>
-  );
+  )
 }
-
-export default App;
