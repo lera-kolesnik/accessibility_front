ab6ae8371ae6fdd14000d7cc36bc2840f0a25b62
Kolesnik Valeria <kolesnik.va8@gmail.com>
2024-02-13 15:58:02 +0000

    added header component with navigation links

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,13 +1,22 @@
-import "./Header.css";
-import { Link } from "react-router-dom";
+import './Header.css'
+import { Link } from 'react-router-dom'
 
 export default function Header() {
   return (
     <header className="Header">
       <div className="Header-wrapper">
-        <Link to="/" className="Header-logo Header-link">A11Y Checker</Link>
-        <Link to="/about-us" className="Header-about Header-link">О проекте</Link>
+        <Link
+          to="/"
+          className="Header-logo Header-link"
+          aria-label="Link to main page"
+        >
+          <span className="Header-link__blue">A11Y</span>{' '}
+          <span className="Header-link__gold">Checker</span>
+        </Link>
+        <Link to="/about-us" className="Header-about Header-link">
+          About project
+        </Link>
       </div>
     </header>
-  );
+  )
 }
