4b5ce39a021f4f0f37db8a136d47d95ccaf7a13c
Kolesnik Valeria <kolesnik.va8@gmail.com>
2024-02-13 16:02:30 +0000

    created both impact and error components

--- src/components/ListErrors/Impacts.jsx
+++ src/components/ListErrors/Impacts.jsx
@@ -1,44 +1,45 @@
-import { useGetNodes } from './hooks'
 import './Impacts.css'
 
-const NAME_IMPACT = {}
-
 export default function Impact({ impact, errors }) {
-  const { formatedNodes } = useGetNodes()
+  if (errors === undefined) return null
 
   return (
     <div className="impact">
-      {Object.values(errors).map(
-        ({ description, help, helpUrl, nodesPosition }) => (
-          <div className="impact__error-group error-group">
-            <div className="error-group__title-wrapper">
-              <p className="error-group__title">{help}</p>
-              <p className="error-group__impact">
-                <div className={`error-group__impact-circle ${impact}`}></div>
-                Importance: {impact}
-              </p>
-            </div>
-            <p className="error-group__description">{description}</p>
-            <p className="error-group__linked">
-              You can read more about the error&nbsp;
-              <a className="error-group__link" target={'_blank'} href={helpUrl}>
-                here
-              </a>
+      {Object.values(errors).map(({ description, help, helpUrl, nodes }) => (
+        <div className="impact__error-group error-group">
+          <div className="error-group__title-wrapper">
+            <p className="error-group__title">{help}</p>
+            <p className="error-group__impact">
+              <div className={`error-group__impact-circle ${impact}`}></div>
+              Importance: {impact}
             </p>
-
-            <ul className="impact__error-list error-list">
-              {nodesPosition.map((position) => (
-                <Error {...formatedNodes[position]} />
-              ))}
-            </ul>
           </div>
-        ),
-      )}
+          <p className="error-group__description">{description}</p>
+          <p className="error-group__linked">
+            You can read more about the error&nbsp;
+            <a
+              className="error-group__link"
+              target={'_blank'}
+              href={helpUrl}
+              rel="noreferrer"
+              aria-label="Read more about this error"
+            >
+              here
+            </a>
+          </p>
+
+          <ul className="impact__error-list error-list">
+            {nodes.map((node) => (
+              <Error {...node} />
+            ))}
+          </ul>
+        </div>
+      ))}
     </div>
   )
 }
 
-function Error({ html, selector, any, screenshots }) {
+function Error({ html, selector, screenshots }) {
   return (
     <li className="error">
       <div className="error__wrapper-img">
@@ -48,6 +49,7 @@ function Error({ html, selector, any, screenshots }) {
             width={100}
             height={80}
             className="error__img"
+            alt=""
           />
         )}
       </div>
